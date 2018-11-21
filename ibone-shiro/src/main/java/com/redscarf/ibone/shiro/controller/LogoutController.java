bf77cd390576c92b5f6c376f2e2df530b78ec9f2
LeeJohn <lidx@communet.io>
2018-11-21 14:56:58 +0800

    调整登录页面

--- ibone-shiro/src/main/java/com/redscarf/ibone/shiro/controller/LogoutController.java
+++ ibone-shiro/src/main/java/com/redscarf/ibone/shiro/controller/LogoutController.java
@@ -6,7 +6,7 @@ import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +17,7 @@ public class LogoutController {
     @Autowired
     IboneConfiguration iboneConfiguration;
 
-    @PostMapping("/logout")
+    @GetMapping("/logout")
     public String loginFail(HttpServletRequest request, HttpServletResponse response, Model model){
         SecurityUtils.getSubject().logout();
         return "redirect:" + iboneConfiguration.getCas().getLoginUrl();
